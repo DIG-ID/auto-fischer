85f079b4aeee713bf0833de1a8748948babf6246
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-10-01 17:45:38 +0100

    up

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ get_header();
 
 <div data-embedded-src="https://www.autoscout24.ch/de/hci/list?design=269&filter=268" class="embedded-content-area"></div>
 
+<iframe src="https://www.autoscout24.ch/de/hci/list?design=269&filter=268" frameborder="0"></iframe>
+
 <script src=" https://www.autoscout24.ch/MVC/Content/as24-hci-desktop/js/e.min.js"></script>
 
 <?php
