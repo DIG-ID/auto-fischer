eb28d9a46ae9e2a698a658c47be53aa42244f4d0
Daniel-Digid <daniel@dig.id>
2024-11-04 11:56:20 +0000

    d

--- template-parts/pages/fahrzeuge/iframe.php
+++ template-parts/pages/fahrzeuge/iframe.php
@@ -4,7 +4,7 @@
 
             <div data-embedded-src="https://www.autoscout24.ch/de/hci/list?design=269&filter=268" class="embedded-content-area"></div>
 
-            <iframe class="w-full" src="https://www.autoscout24.ch/de/hci/list?design=269&filter=268" frameborder="0"></iframe>
+            <!-- <iframe class="w-full" src="https://www.autoscout24.ch/de/hci/list?design=269&filter=268" frameborder="0"></iframe> -->
 
             <script src=" https://www.autoscout24.ch/MVC/Content/as24-hci-desktop/js/e.min.js"></script>
 
