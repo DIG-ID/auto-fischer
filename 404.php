06dc372c8a42f3c3bd2af2523e6409195fd427d9
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-11-05 12:54:06 +0000

    uup

--- 404.php
+++ 404.php
@@ -2,27 +2,25 @@
 get_header();
 do_action( 'before_main_content' );
 ?>
-<section class="page-header bg-blue-shade-5 text-blue-shade-1 pt-28 lg:pt-64 pb-0 lg:-mb-[70px]">
-	<div class="theme-container">
-		<div class="flex flex-col w-full text-center">
-			<div class="page-header--title">
-				<h1 class="font-miller font-light text-5xl xl:text-[250px] text-blue-shade-2 py-5 lg:py-10 invisible"><?php echo esc_html__( '404', 'aare' ) ?></h1>
-			</div>
+
+<section class="section-intro pt-20">
+	<div class="theme-container theme-grid">
+		<div class="col-span-2 md:col-span-6 xl:col-span-12">
+			<?php do_action( 'breadcrumbs' ); ?>
 		</div>
 	</div>
 </section>
-<section class="section-error-404 not-found bg-blue-shade-4 text-blue-shade-1 pt-32 pb-48">
-	<div class="theme-container text-center">
-            <h3 class="text-title-h3 !font-light text-blue-shade-2 mb-12"><?php esc_html_e( 'Page not found', 'aare' ) ?></h3>
-            <?php
-            $booking_url = get_field( 'booking_url', 'options' );
-            if ( $booking_url ) :
-                ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="btn-internal btn-internal--shade-4 !py-3 !px-32 !font-light tracking-[0.2px]"><?php esc_html_e( 'Homepage', 'aare' ); ?></a><?php
-            endif;
-            ?>
-    </div>
-</section>
 
+<section class="section-error-notfound py-12 md:py-16 xl:py-32">
+	<div class="theme-container theme-grid">
+		<div class="col-span-2 md:col-span-6 xl:col-span-8 xl:col-start-3 flex flex-col items-center text-center">
+			<img src="<?php echo get_template_directory_uri() . '/assets/images/404.svg'; ?>" alt="404 illustration" class="max-w-[390px] md:max-w-[447px] xl:max-w-full w-full h-auto object-cover mb-16">
+			<h1 class="title-secondary mb-6"><?php esc_html_e( 'Huch! Sieht aus, als hätten Sie sich verlaufen.', 'auto-fischer' ) ?></h1>
+			<p class="text-body max-w-md mb-6"><?php printf( esc_html__( 'Die von Ihnen gesuchte Seite ist nicht verfügbar. %s Versuchen Sie, die Suche zu wiederholen, oder verwenden Sie die Funktion "Gehe zu".', 'auto-fischer' ), '<br>' ); ?></p>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="btn-main btn-main--empty"><?php esc_html_e( 'Zurück nach Hause', 'auto-fischer' ); ?></a>
+		</div>
+	</div>
+</section>
 <?php
 do_action( 'after_main_content' );
 get_footer();
