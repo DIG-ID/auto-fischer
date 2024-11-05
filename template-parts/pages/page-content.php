ab684b23e854bb52f8c9bf086bad0275b7959518
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-11-05 14:10:08 +0000

    up

--- template-parts/pages/page-content.php
+++ template-parts/pages/page-content.php
@@ -1,9 +1,7 @@
 <section class="section-page-content py-28 overflow-hidden">
-	<div class="theme-container">
-		<div class="theme-grid">
-			<div class="col-span-2 lg:col-start-2 lg:col-span-8">
-				<div class="section-page-content-wrapper"><?php the_content(); ?></div>
-			</div>
+	<div class="theme-container theme-grid">
+		<div class="col-span-2 md:col-span-6 xl:col-start-2 xl:col-span-8">
+			<div class="section-page-content-wrapper"><?php the_content(); ?></div>
 		</div>
 	</div>
 </section>
--- template-parts/pages/page-header.php
+++ template-parts/pages/page-header.php
@@ -1,43 +1,16 @@
-<section class="page-header bg-blue-shade-5 text-blue-shade-1 pt-28 lg:pt-64 pb-20 lg:pb-40">
-	<div class="theme-container">
-		<div class="flex flex-col w-full text-center">
-			<div class="page-header--breadcrumbs invisible fade-in--noscroll">
-				<?php do_action( 'breadcrumbs' ); ?>
-			</div>
-			<div class="page-header--title invisible fade-in--noscroll">
-				<?php
-				$page_header_title = '';
-				if ( is_home() ) :
-					$page_header_title = get_field( 'archive_news_header_title', 'options' );
-				else :
-					$page_header_title = get_field( 'page_header_title' );
-				endif;
-				if ( $page_header_title ) :
-					?>
-					<h1 class="text-title py-5 lg:py-10"><?php echo $page_header_title; ?></h1>
-					<?php
-				else :
-					?>
-					<h1 class="text-title py-5 lg:py-10"><?php the_title(); ?></h1>
-					<?php
-				endif;
-				?>
-			</div>
-			<div class="page-header--description w-3/4 lg:w-[40%] mx-auto invisible fade-in--noscroll">
-				<?php
-				$page_header_desc = '';
-				if ( is_home() ) :
-					$page_header_desc = get_field( 'archive_news_header_description', 'options' );
-				else :
-					$page_header_desc = get_field( 'page_header_description' );
-				endif;
-				if ( $page_header_desc ) :
-					?>
-					<p class="text-description"><?php echo $page_header_desc; ?></p>
-					<?php
-				endif;
-				?>
-			</div>
+<section class="section-intro bg-transparent pt-20 pb-32 relative overflow-hidden">
+	<div class="theme-container theme-grid mb-28">
+		<div class="col-span-2 md:col-span-6 xl:col-span-12">
+			<?php do_action( 'breadcrumbs' ); ?>
+			<h1 class="title-secondary mt-7"><?php echo get_field( 'intro_title' ); ?></h1>
+		</div>
+	</div>
+	<div class="theme-container theme-grid">
+		<div class="col-span-2 md:col-span-3 xl:col-span-5">
+			<h2 class="title-secondary !font-normal"><?php echo get_field( 'intro_subtitle' ); ?></h2>
+		</div>
+		<div class="col-span-2 md:col-span-3 xl:col-span-4 col-start-1 md:col-start-4 xl:col-start-7">
+			<p class="text-body"><?php echo get_field( 'intro_description' ); ?></p>
 		</div>
 	</div>
 </section>
