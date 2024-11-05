ddee7e6daf5bf789f8836d0a14e22cfd2750c2ee
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-11-05 17:47:38 +0000

    up

--- template-parts/pages/page-header.php
+++ template-parts/pages/page-header.php
@@ -1,5 +1,5 @@
-<section class="section-intro bg-transparent pt-20 pb-32 relative overflow-hidden">
-	<div class="theme-container theme-grid mb-28">
+<section class="section-intro bg-transparent pt-20 relative overflow-hidden">
+	<div class="theme-container theme-grid">
 		<div class="col-span-2 md:col-span-6 xl:col-span-12">
 			<?php do_action( 'breadcrumbs' ); ?>
 			<h1 class="title-secondary mt-7"><?php the_title(); ?></h1>
@@ -8,7 +8,7 @@
 	<?php
 	if ( get_field( 'intro_subtitle' ) || get_field( 'intro_description' ) ) :
 		?>
-		<div class="theme-container theme-grid">
+		<div class="theme-container theme-grid mt-28">
 			<div class="col-span-2 md:col-span-3 xl:col-span-5">
 				<h2 class="title-secondary !font-normal"><?php the_field( 'intro_subtitle' ); ?></h2>
 			</div>
