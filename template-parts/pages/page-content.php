e75f74284e288e7f50abd000c35c172919bf4479
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-11-05 17:25:29 +0000

    up

--- template-parts/pages/page-content.php
+++ template-parts/pages/page-content.php
@@ -1,6 +1,6 @@
 <section class="section-page-content py-28 overflow-hidden">
 	<div class="theme-container theme-grid">
-		<div class="col-span-2 md:col-span-6 xl:col-start-2 xl:col-span-8">
+		<div class="col-span-2 md:col-span-6 xl:col-start-2 xl:col-span-10">
 			<div class="section-page-content-wrapper"><?php the_content(); ?></div>
 		</div>
 	</div>
--- template-parts/pages/page-header.php
+++ template-parts/pages/page-header.php
@@ -2,15 +2,22 @@
 	<div class="theme-container theme-grid mb-28">
 		<div class="col-span-2 md:col-span-6 xl:col-span-12">
 			<?php do_action( 'breadcrumbs' ); ?>
-			<h1 class="title-secondary mt-7"><?php echo get_field( 'intro_title' ); ?></h1>
+			<h1 class="title-secondary mt-7"><?php the_title( 'intro_title' ); ?></h1>
 		</div>
 	</div>
-	<div class="theme-container theme-grid">
-		<div class="col-span-2 md:col-span-3 xl:col-span-5">
-			<h2 class="title-secondary !font-normal"><?php echo get_field( 'intro_subtitle' ); ?></h2>
-		</div>
-		<div class="col-span-2 md:col-span-3 xl:col-span-4 col-start-1 md:col-start-4 xl:col-start-7">
-			<p class="text-body"><?php echo get_field( 'intro_description' ); ?></p>
-		</div>
+	<?php
+	if ( get_field( 'intro_subtitle' ) || get_field( 'intro_description' ) ) :
+		?>
+		<div class="theme-container theme-grid">
+			<div class="col-span-2 md:col-span-3 xl:col-span-5">
+				<h2 class="title-secondary !font-normal"><?php the_field( 'intro_subtitle' ); ?></h2>
+			</div>
+			<div class="col-span-2 md:col-span-3 xl:col-span-4 col-start-1 md:col-start-4 xl:col-start-7">
+				<p class="text-body"><?php the_field( 'intro_description' ); ?></p>
+			</div>
 	</div>
+		<?php
+	endif;
+	?>
+
 </section>
