8d9ec56579e04ceb07de6a943f3ef8d1b5c923b8
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-11-05 17:43:16 +0000

    up

--- template-parts/pages/page-header.php
+++ template-parts/pages/page-header.php
@@ -2,7 +2,7 @@
 	<div class="theme-container theme-grid mb-28">
 		<div class="col-span-2 md:col-span-6 xl:col-span-12">
 			<?php do_action( 'breadcrumbs' ); ?>
-			<h1 class="title-secondary mt-7"><?php the_title( 'intro_title' ); ?></h1>
+			<h1 class="title-secondary mt-7"><?php the_title(); ?></h1>
 		</div>
 	</div>
 	<?php
@@ -15,9 +15,8 @@
 			<div class="col-span-2 md:col-span-3 xl:col-span-4 col-start-1 md:col-start-4 xl:col-start-7">
 				<p class="text-body"><?php the_field( 'intro_description' ); ?></p>
 			</div>
-	</div>
+		</div>
 		<?php
 	endif;
 	?>
-
 </section>
