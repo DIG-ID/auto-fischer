f9bc9e3c85ac911f434e7c4c3f9c9cfa52eb83ad
Daniel-Digid <daniel@dig.id>
2024-12-09 12:18:04 +0000

    d

--- template-parts/pages/home/intro.php
+++ template-parts/pages/home/intro.php
@@ -1,14 +1,35 @@
 <section class="section-intro 2xl:min-h-[99vh] bg-light-blue-shade pt-0 md:pt-44 xl:pt-64 pb-12 md:pb-32 xl:pb-[140px] relative overflow-hidden">
-	<?php
-    $heroimage = get_field( 'intro_image' );
-    if ( $heroimage ) :
-        echo wp_get_attachment_image( $heroimage, 'full', false, array( 'class' => 'w-3/5 xl:w-1/2 object-cover absolute top-0 right-0 hidden md:block' ) );
-    endif;
+    <div class="absolute top-0 right-0 w-3/5 xl:w-1/2 hidden md:block">
+        <div class="relative">
+        <?php
+            $heroimage = get_field( 'intro_image' );
+            if ( $heroimage ) :
+                echo wp_get_attachment_image( $heroimage, 'full', false, array( 'class' => 'object-cover w-full' ) );
+            endif;
+        ?>
+        <?php 
+        $award_logo = get_field('intro_award_logo');
+        $award_image = get_field( 'intro_award_image' );
+        if ($award_logo && $award_image) :
+            echo wp_get_attachment_image( $award_image, 'full', false, array( 'class' => 'max-w-[125px] lg:max-w-[200px] max-h-[204px] absolute -bottom-16 lg:-bottom-24 right-12 lg:right-16' ) );
+        endif;
+        ?>
+        </div>
+    </div>
+    <?php
     $heroimage_m = get_field( 'intro_image_mobile' );
     if ( $heroimage_m ) :
         echo wp_get_attachment_image( $heroimage_m, 'full', false, array( 'class' => 'w-full object-cover relative md:hidden' ) );
     endif;
     ?>
+    <?php 
+    $award_logo = get_field('intro_award_logo');
+    $award_image = get_field( 'intro_award_image' );
+    if ($award_logo && $award_image) :
+        echo wp_get_attachment_image( $award_image, 'full', false, array( 'class' => 'max-w-[100px] max-h-[104px] absolute block md:hidden top-6  right-5' ) );
+    endif;
+    ?>
+    
     <div class="theme-container theme-grid relative z-30">
         <div class="col-span-2 md:col-span-5 xl:col-span-6 mb-12">
             <p class="title-pre text-darker-blue-shade mt-5 md:mt-0"><?php the_field( 'intro_pre_title' ); ?></p>
