c36447e380f5e5e7fbf3b2bb24a64f86a7e14699
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-12-10 15:14:57 +0000

    up

--- functions.php
+++ functions.php
@@ -94,9 +94,6 @@ function fischer_theme_enqueue_styles() {
 		wp_enqueue_script( 'google-map-settings', get_stylesheet_directory_uri() . '/assets/js/google-maps.js', array( 'jquery' ), $theme_version, true );
 		wp_enqueue_script( 'google-map-api', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCB2RShyxiN7xPsQy1QI_SbqXXjW5p08S0&callback=initMap', array(), $theme_version, true );
 	endif;
-	if ( is_page_template( 'page-templates/page-ankauf.php' ) ) :
-		//wp_enqueue_script( 'cf7-multifile-upload', get_template_directory_uri() . '/assets/js/cf7/cf7-multifile-upload.js', array(), $theme_version, true );
-	endif;
 }
 
 add_action( 'wp_enqueue_scripts', 'fischer_theme_enqueue_styles' );
@@ -139,9 +136,6 @@ require get_template_directory() . '/inc/theme-admin-settings.php';
 
 // The theme custom menu walker settings.
 require get_template_directory() . '/inc/theme-custom-menu-walker.php';
-
-// The theme custom menu walker settings.
-//require get_template_directory() . '/inc/cf7-multifile-upload.php';
 
 function my_console_log(...$data) {
 	$json = json_encode($data);
