66427b538118a2397ce03dcefc16c991e2d4b883
Daniel-Digid <daniel@dig.id>
2025-01-09 11:57:30 +0000

    map key

--- functions.php
+++ functions.php
@@ -92,7 +92,7 @@ function fischer_theme_enqueue_styles() {
 	wp_enqueue_script( 'theme-scripts', get_stylesheet_directory_uri() . '/dist/js/main.js', array( 'jquery' ), $theme_version, true );
 	if ( is_page_template( 'page-templates/page-kontakt.php' ) || is_admin() ) :
 		wp_enqueue_script( 'google-map-settings', get_stylesheet_directory_uri() . '/assets/js/google-maps.js', array( 'jquery' ), $theme_version, true );
-		wp_enqueue_script( 'google-map-api', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCB2RShyxiN7xPsQy1QI_SbqXXjW5p08S0&callback=initMap', array(), $theme_version, true );
+		wp_enqueue_script( 'google-map-api', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBAZN5TfX1aWmjodZ4e_6sOcaJV4D59jfo&callback=initMap', array(), $theme_version, true );
 	endif;
 }
 
@@ -101,7 +101,7 @@ add_action( 'wp_enqueue_scripts', 'fischer_theme_enqueue_styles' );
 //Google Map Init
 function fischer_theme_google_map_init() {
 	if ( is_admin() ) :
-		acf_update_setting( 'google_api_key', 'AIzaSyCB2RShyxiN7xPsQy1QI_SbqXXjW5p08S0' );
+		acf_update_setting( 'google_api_key', 'AIzaSyBAZN5TfX1aWmjodZ4e_6sOcaJV4D59jfo' );
 	endif;
 }
 
