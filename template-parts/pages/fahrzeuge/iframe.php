2c8b5a3bc51e975553d9bb561fbf93bb5fc37358
Daniel-Digid <daniel@dig.id>
2025-04-14 12:07:12 +0100

    Update iframe.php

--- template-parts/pages/fahrzeuge/iframe.php
+++ template-parts/pages/fahrzeuge/iframe.php
@@ -10,4 +10,11 @@
 
         </div>
     </div>
+    <div class="theme-container theme-grid mb-28">
+        <div class="col-span-2 md:col-span-6 xl:col-span-12">
+
+            <div class="hci-container" data-config-id="2876" data-language="de" data-entry-point="search"></div><script src="https://www.autoscout24.ch/assets/hci/v2/hci.current.js"></script>
+
+        </div>
+    </div>
 </section>
