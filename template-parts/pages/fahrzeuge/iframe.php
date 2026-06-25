f5f77b96208f4a534cf1d0c8a9243cb256f9c163
Daniel-Digid <daniel@dig.id>
2026-06-25 14:05:27 +0100

    Update iframe.php

--- template-parts/pages/fahrzeuge/iframe.php
+++ template-parts/pages/fahrzeuge/iframe.php
@@ -17,7 +17,7 @@
     <div class="theme-container theme-grid mb-28">
         <div class="col-span-2 md:col-span-6 xl:col-span-12">
 
-            <?php echo do_shortcode('[as24ci_archive]'); ?>
+            <?php echo do_shortcode( get_field( 'cars_shortcode' ) ); ?>
 
             <?php /*
             <div class="hci-container" data-config-id="2849" data-language="de" data-entry-point="search"></div>
